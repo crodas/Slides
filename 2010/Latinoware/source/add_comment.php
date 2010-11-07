f1bb103b1652173b4d312c00960953e014b1e56d
César D. Rodas <crodas@php.net>
2010-11-07 06:39:08 -0300

    + Fixed minor issues + TODO: Migration

--- 2010/Latinoware/source/add_comment.php
+++ 2010/Latinoware/source/add_comment.php
@@ -8,12 +8,11 @@ if (empty($news)) throw new Exception("not valid news");
 $new_comment = array(
     'name' => $_SESSION['user']['name'],
     'website' => $_SESSION['user']['website'],
-    'text' => $_POST['text'],
-    'news' => $_POST['news_id'],
+    'text' => $_POST['text'], 'news' => $_POST['news_id'],
 );
 $db->comment->save($new_comment);
+
 $update = array('$inc' => array('totalComments' => 1));
-if ($news['totalComments'] < 10) {
+if ($news['totalComments'] < 10)
     $update['$push'] = array('comments' => $new_comment);
-}
 $db->news->update($news, $update);
